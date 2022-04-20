a3abf505894d701b1f4e9045107a99deb483cc13
thais <thaisviana081@outlook.com>
2022-04-20 16:58:47 -0300

    requisito 5

---
+++ script.js
@@ -0,0 +1,9 @@
+const selecting = document.querySelector('#pixel-board');
+for (let index = 0; index < 25; index += 1) {
+  let generatePixel = index;
+  let create = document.createElement('div');
+  create.generatePixel;
+  create.classList.add('pixel');
+  selecting.appendChild(create);
+}
+
