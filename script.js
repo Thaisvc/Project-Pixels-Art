87cbdf52d23980d89691cb739a10e1b52b023c5c
thais <thaisviana081@outlook.com>
2022-04-21 15:48:18 -0300

    ajuste 8 ok

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
+window.onload = preto;
 const selecting = document.querySelector('#pixel-board');
 const pixels = document.getElementsByClassName('pixel');
+const selecionado = document.querySelectorAll('.selected')[0];
 
-let selected = document.getElementsByClassName('selected')[0];
+function preto (event) {
+  let selecionado = document.querySelectorAll('.selected')[0];
+  if (selecionado) {
+    const cssObj = window.getComputedStyle(selecionado, null);
+    let bgColor = cssObj.getPropertyValue('background-color');
+    /* document.getElementById('demo').innerHTML = bgColor; */
+    for (let key in pixels) {
+      let block = pixels[key];
+      block.addEventListener('click', function (event) {
+        event.target.style.backgroundColor = bgColor;
+      });
+    }
+  }
+}
 
-
-
-const cssObj = window.getComputedStyle(selected, null);
-let bgColor = cssObj.getPropertyValue("background-color");
-
- 
-
+selected = document.addEventListener('click',function (event) {
+    if (event.target.classList.contains('selected')) {
+      let selected = document.querySelectorAll('.selected')[0];
+      const cssObj = window.getComputedStyle(selected, null);
+      let bgColor = cssObj.getPropertyValue('background-color');
+      for (let key in pixels) {
+        let block = pixels[key];
+        block.addEventListener('click', function (event) {
+          event.target.style.backgroundColor = bgColor;
+        });
+      }
+    }
+  },
+  false);
 
 for (let index = 0; index < 25; index += 1) {
-  let generatePixel = index;
   let create = document.createElement('div');
   create.generatePixel;
   create.classList.add('pixel');
   selecting.appendChild(create);
 }
+
 function colorPlate() {
   paleta = document.getElementsByClassName('color');
   for (let index = 0; index < paleta.length; index += 1) {
@@ -30,10 +52,4 @@ function colorPlate() {
 }
 colorPlate();
 
-for (let key in pixels) {
-  let block = pixels[key];
-  block.addEventListener('click', function (event) {
-    event.target.style.backgroundColor = bgColor;
-  });
-}
- 
+
